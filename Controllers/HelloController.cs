1ba685de7e7cb034665726bd9c00b74105e6c68a
Sophie_BRULTET <sophie.brultet@gmail.com>
2020-11-23 12:44:30 +0100

    Quete_Routage

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quete_Routage.Controllers
+{
+    //Routage par attribut avec endpoints.MapControllers()
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        [Route("sayhello")]
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        [HttpGet]
+        [Route("yellhello")]
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+    }
+}
---
+++ Controllers/HelloConvController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quete_Routage.Controllers
+{
+    public class HelloConvController
+    {
+        //Routage par convention
+        public String Say()
+        {
+            return "Hello !";
+        }
+
+        public String Yell()
+        {
+            return "HELLO !";
+        }
+    }
+}
